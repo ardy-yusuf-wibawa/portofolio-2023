be673a2d429af12e383cb6157d7602140e861340
ardy yusuf <ardyyusufw0391@gmail.com>
2023-09-26 23:53:42 +0700

    feat: loader

--- src/components/CardComp.jsx
+++ src/components/CardComp.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Card, Badge } from 'flowbite-react'
 import { Link } from 'react-router-dom'
+import DoorDashFavorite from '../utils/loader'
 
 const techStackColors = {
   HTML: 'red',
@@ -12,7 +13,6 @@ const techStackColors = {
   Typescript: 'yellow',
   MySQL: 'purple',
   'React Native': 'red'
-  // Add more techStack stacks and colors as needed
 }
 
 const TechStackTag = ({ techStack }) => {
@@ -50,6 +50,7 @@ const LinkPath = ({ paths }) => {
 const CardComp = () => {
   const apiProject = process.env.REACT_APP_GOOGLE_SPREADSHEET_API_PROJECT
   const [projectData, setProjectData] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchProjectData = async () => {
@@ -65,41 +66,58 @@ const CardComp = () => {
         }))
 
         setProjectData(projectsWithArrays)
+        setLoading(false)
       } catch (error) {
         console.error('error fetching data:', error)
+        setLoading(false)
       }
     }
     fetchProjectData()
   }, [])
 
   return (
-    <div className='lg:w-(90%) container mx-auto grid w-[1/4] grid-rows-1 gap-[4vw] px-4 pb-10 sm:w-full sm:grid-cols-2 sm:gap-[2.3vw]  sm:py-10'>
-      {projectData.map((value, index) => (
-        <Card
-          key={index}
-          imgAlt={value.name}
-          imgSrc={value.img}>
-          <h5 className='flex items-start justify-start text-base font-bold tracking-tight text-gray-900 dark:text-white sm:text-2xl'>
-            {value.name}
-          </h5>
-          <p className='hidden text-ellipsis font-normal text-gray-700 dark:text-gray-400 sm:block'>
-            {value.desc}
-          </p>
-          <LinkPath paths={value.paths} />
-          <div className='container grid max-w-[35rem] gap-1 sm:gap-2 lg:grid-cols-5'>
-            {value.techStack.map(
-              (techStack, index) =>
-                index < 5 && (
-                  <TechStackTag
-                    key={index}
-                    techStack={techStack}
-                  />
-                )
-            )}
-          </div>
-        </Card>
-      ))}
-    </div>
+    <>
+      <div className='lg:w-(90%) container mx-auto grid w-[1/4] grid-rows-1 gap-[4vw] px-4 pb-10 sm:w-full sm:grid-cols-2 sm:gap-[2.3vw]  sm:py-10'>
+        {loading ? (
+          <>
+            <DoorDashFavorite />
+            <DoorDashFavorite />
+            <DoorDashFavorite />
+            <DoorDashFavorite />
+            <DoorDashFavorite />
+            <DoorDashFavorite />
+          </>
+        ) : (
+          <>
+            {projectData.map((value, index) => (
+              <Card
+                key={index}
+                imgAlt={value.name}
+                imgSrc={value.img}>
+                <h5 className='flex items-start justify-start text-base font-bold tracking-tight text-gray-900 dark:text-white sm:text-2xl'>
+                  {value.name}
+                </h5>
+                <p className='hidden text-ellipsis font-normal text-gray-700 dark:text-gray-400 sm:block'>
+                  {value.desc}
+                </p>
+                <LinkPath paths={value.paths} />
+                <div className='container grid max-w-[35rem] gap-1 sm:gap-2 lg:grid-cols-5'>
+                  {value.techStack.map(
+                    (techStack, index) =>
+                      index < 5 && (
+                        <TechStackTag
+                          key={index}
+                          techStack={techStack}
+                        />
+                      )
+                  )}
+                </div>
+              </Card>
+            ))}
+          </>
+        )}
+      </div>
+    </>
   )
 }
 
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import { List } from 'react-content-loader'
 
 const TimeLine = () => {
   const [timeLineData, setTimeLineData] = useState([])
+  const [loading, setLoading] = useState(true)
   const apiKey = process.env.REACT_APP_GOOGLE_SPREADSHEET_API
 
   useEffect(() => {
@@ -10,8 +12,10 @@ const TimeLine = () => {
         const response = await fetch(apiKey)
         const data = await response.json()
         setTimeLineData(data)
+        setLoading(false)
       } catch (error) {
         console.error('Error fetching data:', error)
+        setLoading(false)
       }
     }
 
@@ -20,41 +24,45 @@ const TimeLine = () => {
 
   return (
     <>
-      <ol className='relative border-l border-gray-200 pt-10 dark:border-gray-700'>
-        <span className='flex p-5 text-xl font-bold'>Experience</span>
-        {timeLineData.map((value, index) => (
-          <li
-            key={index}
-            className='mb-10 ml-6'>
-            <span className='absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-100 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900'>
-              <svg
-                className='h-3 w-3 text-blue-800 dark:text-blue-300'
-                fill='currentColor'
-                viewBox='0 0 20 20'
-                xmlns='http://www.w3.org/2000/svg'>
-                <path
-                  fillRule='evenodd'
-                  d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
-                  clipRule='evenodd'></path>
-              </svg>
-            </span>
-            <h3 className='mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white'>
-              {value.title}
-              {value.event.trim() !== '' && (
-                <span className='ml-3 mr-2 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300'>
-                  {value.event}
-                </span>
-              )}
-            </h3>
-            <time className='mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500'>
-              on {value.date_start} - {value.date_end}
-            </time>
-            <p className='mb-4 text-left text-base font-normal text-gray-500 dark:text-gray-400'>
-              {value.desc}
-            </p>
-          </li>
-        ))}
-      </ol>
+      {loading ? (
+        <List />
+      ) : (
+        <ol className='relative border-l border-gray-200 pt-10 dark:border-gray-700'>
+          <span className='flex p-5 text-xl font-bold'>Experience</span>
+          {timeLineData.map((value, index) => (
+            <li
+              key={index}
+              className='mb-10 ml-6'>
+              <span className='absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-100 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900'>
+                <svg
+                  className='h-3 w-3 text-blue-800 dark:text-blue-300'
+                  fill='currentColor'
+                  viewBox='0 0 20 20'
+                  xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    fillRule='evenodd'
+                    d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
+                    clipRule='evenodd'></path>
+                </svg>
+              </span>
+              <h3 className='mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white'>
+                {value.title}
+                {value.event.trim() !== '' && (
+                  <span className='ml-3 mr-2 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300'>
+                    {value.event}
+                  </span>
+                )}
+              </h3>
+              <time className='mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500'>
+                on {value.date_start} - {value.date_end}
+              </time>
+              <p className='mb-4 text-left text-base font-normal text-gray-500 dark:text-gray-400'>
+                {value.desc}
+              </p>
+            </li>
+          ))}
+        </ol>
+      )}
     </>
   )
 }
